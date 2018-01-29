f33f9010e56755b891dcdc9330f3a5b59da32262
Wyatt Clement <wyclem@umich.edu>
2018-01-29 12:25:40 -0500

    Wrote code to check if user input ends in ? and to keep asking for a question until user types quit

--- magic_eight.py
+++ magic_eight.py
@@ -1,3 +1,9 @@
 def getUserQuestion():
-	user_question = input("What is you question?")
-	return user_question
+    user_question = input("What is your question?")
+    return user_question
+
+question = getuserQuestion()
+while question != 'quit':
+    if question[-1] != "?":
+        print("I'm sorry, I can only answer questions.")
+    question = getUserQuestion()
