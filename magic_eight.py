20c7d756f4d9f9f77959207c9012dca472fbbbba
Jeremy Wolverton <jjwolver@umich.edu>
2018-01-30 20:39:54 -0500

    Fixed line 5, capital U in user was required for me to run

--- magic_eight.py
+++ magic_eight.py
@@ -2,7 +2,7 @@ def getUserQuestion():
     user_question = input("What is your question?")
     return user_question
 
-question = getuserQuestion()
+question = getUserQuestion()
 while question != 'quit':
     if question[-1] != "?":
         print("I'm sorry, I can only answer questions.")
